1fb519dca6e621cf23693a8289c20fac11340c44
Matthew <ashlander@live.com>
2021-02-19 02:29:43 -0600

    Marker size and colors working

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,21 +9,40 @@ function createFeatures(data) {
 	// console.log(data);
 	var mag = [];
 	var location = [];
+	var depth = [];
+	var place = [];
+	var date = [];
 
 	for (i = 0; i < Object.keys(data).length; i++) {
 		var lat = data[i].geometry.coordinates[0];
 		var lon = data[i].geometry.coordinates[1];
-		var depth = data[i].geometry.coordinates[2];
-		var place = data[i].properties.place;
 		var unix = data[i].properties.time;
-		var date = Date(unix*1000);
-		
+		var convertedUnix = Date(unix*1000);
+
+		depth.push(data[i].geometry.coordinates[2]);
+		place.push(data[i].properties.place);
+		date.push(convertedUnix);
 		mag.push(data[i].properties.mag);
 		location.push([lon, lat]);
 	};
 	console.log(data);
 	console.log(mag);
 
+	function depthColor(depth) {
+		if (depth > 10) {
+			return '#390606';
+		} else if (depth > 8) {
+			return '#4a0707';
+		} else if (depth > 6) {
+			return '#5c0909';
+		} else if (depth > 4) {
+			return '#6e0b0b';
+		} else if (depth > 2) {
+			return '#800d0d';
+		} else if (depth <= 2) {
+			return '#920f0f';
+		};
+	};
 	// Create a map object
 	var myMap = L.map("mapid", {
 		center: [0, 0],
@@ -44,11 +63,12 @@ function createFeatures(data) {
 		L.circle(location[i], {
 		fillOpacity: 0.75,
 		weight: 0,
-		fillColor: depth,
+		fillColor: depthColor(depth[i]),
 		radius: (mag[i] * 25000)
-		}).bindPopup(`Date: ${date}<br/>
+		}).bindPopup(`Date: ${date[i]}<br/>
+					Location: ${place[i]}<br/>
 					Magnitude: ${mag[i]}<br/>
-					Depth: ${depth}`).addTo(myMap);
+					Depth: ${depth[i]}`).addTo(myMap);
 	}
 }
   
